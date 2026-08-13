d4b1e3325fa923194b3333e0f6298cf38483ce93
Jihed <houichijihed63@gmaill.com>
2026-08-13 11:30:18 +0200

    fix: add .gitignore and remove build artifacts from tracking

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/InvoiceExtractor.dir/InvoiceExtractor_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/InvoiceExtractor.dir/InvoiceExtractor_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- C:\Users\Lenovo\Desktop\satge\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\InvoiceExtractor_autogen\EWIEGA46WW\qrc_resources.cpp
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/InvoiceExtractor.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for InvoiceExtractor.
-# This may be replaced when dependencies are built.
